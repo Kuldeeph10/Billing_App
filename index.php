ecb93683017e2ee377a93554ef2efddefb235014
kuldeep <kuldeephawladar14@gmail.com>
2025-08-04 00:09:22 +0530

    index.html and include folder createrd for header,footer,nav

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("includes/header.php"); ?>
+
+<div class="py-5">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Billing-Application</h1>
+                <a href="login.php" class="btn btn-primary mt-4">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
+<h1></h1>
+<?php include("includes/footer.php"); ?>
+
